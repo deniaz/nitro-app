6b2762a914f62500090401c149c98c05e40fea91
Robert Vogt <robert.vogt@namics.com>
2016-01-08 22:19:49 +0100

    Add handlebars wrapper and empty component helper

---
+++ src/hbs/component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function renderComponent(name, data) {
+
+}
+
+module.exports = renderComponent;
---
+++ src/hbs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var hbs = require('hbs');
+var hbsUtils = require('hbs-utils')(hbs);
+
+function start(app, config) {
+	hbsUtils.registerWatchedPartials(config.nitro.view_partials_directory);
+	hbs.registerHelper('component', require('./component'));
+
+	app.engine(config.nitro.view_file_extension, hbs.__express);
+}
+
+module.exports = {
+	start: start
+};
