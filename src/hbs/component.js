288979e1b000a60f02de0951dfed279ba644e658
Robert Vogt <robert.vogt@namics.com>
2016-01-08 22:32:34 +0100

    Add simple component helper

--- src/hbs/component.js
+++ src/hbs/component.js
@@ -1,7 +1,32 @@
 'use strict';
 
-function renderComponent(name, data) {
+var fs = require('fs');
+var path = require('path');
+var hbs = require('hbs');
 
+function createHelper(config) {
+	return function(name, data) {
+		for (var key in config.nitro.components) {
+			var component = config.nitro.components[key];
+			if (component.hasOwnProperty('path')) {
+				var file = name + '.' + config.nitro.view_file_extension;
+				var fullPath = path.join(
+					config.nitro.base_path,
+					component.path,
+					'/',
+					name,
+					'/',
+					file
+				);
+
+				if (fs.existsSync(fullPath)) {
+					return new hbs.handlebars.SafeString(
+						hbs.handlebars.compile(fs.readFileSync(fullPath, 'utf-8')).call()
+					);
+				}
+			}
+		}
+	};
 }
 
-module.exports = renderComponent;
+module.exports = createHelper;
--- src/hbs/index.js
+++ src/hbs/index.js
@@ -5,7 +5,7 @@ var hbsUtils = require('hbs-utils')(hbs);
 
 function start(app, config) {
 	hbsUtils.registerWatchedPartials(config.nitro.view_partials_directory);
-	hbs.registerHelper('component', require('./component'));
+	hbs.registerHelper('component', require('./component')(config));
 
 	app.engine(config.nitro.view_file_extension, hbs.__express);
 }
